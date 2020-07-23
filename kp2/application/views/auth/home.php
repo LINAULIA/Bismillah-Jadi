4a2722ec606dbe006a81f598dd583ef01aa0dfc7
LINAULIA <51009783+LINAULIA@users.noreply.github.com>
2020-07-23 20:12:00 +0700

    Menambahkan folder Kp2 tampilan home

---
+++ kp2/application/views/auth/home.php
@@ -0,0 +1,123 @@
+<!-- navbar -->
+<nav class="navbar navbar-default navbar-static-top">
+    <div class="container" id="navbar">
+        <div class="navbar-header">
+            <a href="#" class="navbar-brand navbar-link"><img src="assets/img/logo-SV.png" /></a>
+            <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1">
+                <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse" id="navcol-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li role="presentation"><a href="#jumbotron">HOME</a></li>
+                <li role="presentation"><a href="#Info Pelatihan">Info Pelatihan</a></li>
+                <li role="presentation"><a href="#Login">Login</a></li>
+                <li role="presentation"><a href="#Kontak Kami">Kontak Kami</a></li>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- navbar -->
+
+<!-- jumbotron -->
+<div id="jumbotron">
+    <div class="jumbotron">
+        <img src="assets/img/gedungsv.jpg" height="550px;" style="float:center;" />
+    </div>
+</div>
+<!-- jumbotron -->
+
+<!-- container atas -->
+
+<!-- container bawah -->
+
+<!-- container bawah -->
+
+<!-- container About -->
+<div id="about">
+    <div class="container row">
+        <div class="col-md-6">
+            <img src="assets/img/about2.jpg" height="300px;" style="float: left;" />
+        </div>
+        <div class="col-md-6" style="padding-left: 30px;">
+            <h1 style="padding: 3%;">
+                <strong> Training Info Sekolah Vokasi</strong>
+            </h1>
+            <p style="text-align: justify;">
+                Ini merupakan paragraf penjelasan training info sekolah vokasi</p>
+        </div>
+
+    </div>
+</div>
+<!-- container About -->
+
+<!-- gallery -->
+<div id="fasilitas">
+    <div class="container">
+        <h1>Fasilitas</h1>
+        <div class="row">
+            <div class="col-xs-offset-1 col-xs-10">
+                <div class="row">
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="fasilitas-tile">
+                            <div class="fasilitas-detail">
+                                <h4 class="tile-title text-primary"><strong>Trainer</strong></h4>
+                                <p>Diajar akademisi/praktisi SV Undip atau mitra industri yang kompeten</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="fasilitas-tile">
+                            <div class="fasilitas-detail">
+                                <h4 class="tile-title text-primary"><strong>Kurikulum</strong></h4>
+                                <p>Aplikatif, tepat sasaran sesuai kebutuhan</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="fasilitas-tile">
+                            <div class="fasilitas-detail">
+                                <h4 class="tile-title text-primary"><strong>Modul</strong></h4>
+                                <p>Modul lengkap dan mudah dipelajari</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="fasilitas-tile">
+                            <div class="fasilitas-detail">
+                                <h4 class="tile-title text-primary"><strong>Sarana</strong></h4>
+                                <p>Kelas/laboratorium pelatihan nyaman dan memadahi</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="fasilitas-tile">
+                            <div class="fasilitas-detail">
+                                <h4 class="tile-title text-primary"><strong>Internet</strong></h4>
+                                <p>High speed internet access dari wifi SV Undip</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="fasilitas-tile">
+                            <div class="fasilitas-detail">
+                                <h4 class="tile-title text-primary"><strong>Ekonomis</strong></h4>
+                                <p>Biaya fleksibel dan terjangkau</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="fasilitas-tile">
+                            <div class="fasilitas-detail">
+                                <h4 class="tile-title text-primary"><strong>Sertifikat</strong></h4>
+                                <p>Diterbitkan SV Undip dan/atau lembaga sertifikasi bereputasi</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- gallery -->
---
+++ kp2/application/views/templates/auth_footer.php
@@ -0,0 +1,25 @@
+<!-- about -->
+<div id="about">
+    <div class="container footer">
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <h1>HUBUNGI KAMI </h1>
+            <div class="contact-details">
+                <p>Jalan Prof. Soedarto, SH Tembalang Semarang, Kotak Pos 1269, Indonesia</p>
+                <p>Phone: </p>
+                <p>SMS: </p>
+                <p>WhatsApp: </p>
+                <p>Telepon: </p>
+                <p>Fax: </p>
+                <p>Email: </p>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- about -->
+
+<!-- kaki -->
+<div id="kaki">
+    <div class="container">
+        </body>
+
+        </html>
